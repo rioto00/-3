74cc2dcabf18942801aba7c78935fad7e7ce258c
EC2 Default User <ec2-user@ip-172-31-0-93.ap-northeast-1.compute.internal>
2024-03-10 05:38:04 +0000

    7a

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,9 @@
 class BooksController < ApplicationController
   def index
     @user = current_user
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
+    @books = Book.includes(:favorites).sort_by { |book| -book.favorites.where(created_at: from...to).count }
     @book = Book.new
     @books = Book.all
   end
